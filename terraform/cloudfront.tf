58caac59590e5e360fd1a503ae5317cc86c7a910
ryudai <bigdragon.06102001@gmail.com>
2025-04-28 22:16:41 +0900

    CloudFrontのVPCオリジンとキャッシュポリシーを追加し、ALBのセキュリティグループ設定を更新

---
+++ terraform/cloudfront.tf
@@ -0,0 +1,107 @@
+resource "aws_cloudfront_vpc_origin" "alb" {
+  vpc_origin_endpoint_config {
+    name                   = "alb-origin"
+    arn                    = aws_lb.web.arn
+    http_port              = 80
+    https_port             = 443
+    origin_protocol_policy = "http-only"
+
+    origin_ssl_protocols {
+      items    = ["TLSv1.2"]
+      quantity = 1
+    }
+  }
+}
+
+resource "aws_cloudfront_distribution" "alb" {
+  origin {
+    domain_name = aws_lb.web.dns_name
+    origin_id   = "alb-origin"
+    vpc_origin_config {
+      vpc_origin_id = aws_cloudfront_vpc_origin.alb.id
+    }
+  }
+
+  enabled = true
+
+  default_cache_behavior {
+    viewer_protocol_policy   = "allow-all"
+    allowed_methods          = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods           = ["GET", "HEAD"]
+    cache_policy_id          = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad" # Managed-CachingDisabled
+    target_origin_id         = "alb-origin"
+    origin_request_policy_id = "216adef6-5c7f-47e4-b989-5492eafa07d3" # Managed-AllViewer
+  }
+
+  ordered_cache_behavior {
+    path_pattern     = "/favicon.ico"
+    target_origin_id = "alb-origin"
+
+    viewer_protocol_policy = "allow-all"
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+  }
+  ordered_cache_behavior {
+    path_pattern     = "/js/*"
+    target_origin_id = "alb-origin"
+
+    viewer_protocol_policy = "allow-all"
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+  }
+  ordered_cache_behavior {
+    path_pattern     = "/css/*"
+    target_origin_id = "alb-origin"
+
+    viewer_protocol_policy = "allow-all"
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+  }
+  ordered_cache_behavior {
+    path_pattern     = "/image/*"
+    target_origin_id = "alb-origin"
+
+    viewer_protocol_policy = "allow-all"
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods         = ["GET", "HEAD"]
+    cache_policy_id        = aws_cloudfront_cache_policy.image.id
+  }
+
+  price_class = "PriceClass_All"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+resource "aws_cloudfront_cache_policy" "image" {
+  name = "image-cache-policy"
+
+  default_ttl = 60
+  max_ttl     = 60
+  min_ttl     = 60
+
+  parameters_in_cache_key_and_forwarded_to_origin {
+    cookies_config {
+      cookie_behavior = "none"
+    }
+
+    headers_config {
+      header_behavior = "none"
+    }
+
+    query_strings_config {
+      query_string_behavior = "none"
+    }
+  }
+}
--- terraform/sg.tf
+++ terraform/sg.tf
@@ -1,18 +1,14 @@
+data "aws_security_group" "vpc_origin_sg" {
+  depends_on = [aws_cloudfront_vpc_origin.alb]
+  filter {
+    name   = "group-name"
+    values = ["CloudFront-VPCOrigins-Service-SG"]
+  }
+}
+
 resource "aws_security_group" "alb" {
   name   = "Private-isu-alb"
   vpc_id = aws_vpc.vpc.id
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["${var.your_ip}"]
-  }
-  ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.benchmark.id]
-  }
 
   egress {
     from_port        = 0
@@ -21,6 +17,17 @@ resource "aws_security_group" "alb" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+}
+
+# 循環参照を避けるためにdepends_onを使用
+resource "aws_vpc_security_group_ingress_rule" "alb" {
+  depends_on = [data.aws_security_group.vpc_origin_sg]
+
+  security_group_id            = aws_security_group.alb.id
+  from_port                    = 80
+  to_port                      = 80
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = data.aws_security_group.vpc_origin_sg.id
 }
 
 resource "aws_security_group" "web" {
